b0fcac5b890cfe0824c57b730014799aeaa2ff5c
mmelograno <mmelograno@devspark.com>
2016-05-09 11:01:25 -0300

    added apidoc

--- routes/episodes.js
+++ routes/episodes.js
@@ -4,7 +4,41 @@ const express = require('express');
 const router = express.Router();
 const episodes = require('../controllers/episodes.controller');
 
+/**
+ * @api {get} /episodes/ Request Episodes
+ * @apiVersion 0.0.1
+ * @apiName GetEpisodes
+ * @apiGroup Episode
+ * @apiExample {curl} Example usage:
+ *     curl -i http://localhost/episodes/
+ *
+ * @apiSuccess (200) {Object[]} episodes Episodes stored in the app.
+ * @apiSuccess (200) {String}   episodes._id   Object id.
+ * @apiSuccess (200) {String}   episodes.title   Title.
+ * @apiSuccess (200) {String}   episodes.description   Description.
+ * @apiSuccess (200) {Number}   episodes.season   Season number.
+ * @apiSuccess (200) {Number}   episodes.episode   Episode number.
+ * @apiSuccess (200) {Number}   episodes.duration   Duration.
+ * @apiSuccess (200) {Object}   episodes._show   Show related.
+ * @apiSuccess (200) {String}   episodes._show._id   Object id.
+ * @apiSuccess (200) {String}   episodes._show.name   Name of the show.
+ */
 router.get('/', episodes.getEpisodes);
+/**
+ * @api {post} /episodes/ Add Episode
+ * @apiVersion 0.0.1
+ * @apiName AddEpisode
+ * @apiGroup Episode
+ *
+ * @apiSuccess (200) {Object} episode Episodes stored in the app.
+ * @apiSuccess (200) {String}   episode._id   Object id.
+ * @apiSuccess (200) {String}   episode.title   Title.
+ * @apiSuccess (200) {String}   episode.description   Description.
+ * @apiSuccess (200) {Number}   episode.season   Season number.
+ * @apiSuccess (200) {Number}   episode.episode   Episode number.
+ * @apiSuccess (200) {Number}   episode.duration   Duration.
+ * @apiSuccess (200) {String}   episode._show   Object id.
+ */
 router.post('/', episodes.addEpisode);
 
 module.exports = router;
--- routes/shows.js
+++ routes/shows.js
@@ -4,7 +4,33 @@ const express = require('express');
 const router = express.Router();
 const shows = require('../controllers/shows.controller');
 
+/**
+ * @api {get} /shows/ Request Shows
+ * @apiVersion 0.0.1
+ * @apiName GetShows
+ * @apiGroup Show
+ * @apiExample {curl} Example usage:
+ *     curl -i http://localhost/shows/
+ *
+ * @apiSuccess (200) {Object[]} shows Shows stored in the app.
+ * @apiSuccess (200) {String}   shows._id   Object id.
+ * @apiSuccess (200) {String}   shows.name   Title.
+ * @apiSuccess (200) {String}   shows.description   Description.
+ */
 router.get('/', shows.getShows);
+/**
+ * @api {post} /shows/ Add Show
+ * @apiVersion 0.0.1
+ * @apiName AddShow
+ * @apiGroup Show
+ *
+ * @apiSuccess (200) {Object} show Show added.
+ * @apiSuccess (200) {String}   show._id   Object id.
+ * @apiSuccess (200) {String}   show.name   Title.
+ * @apiSuccess (200) {String}   show.description   Description.
+ *
+ * @apiError (400) InvalidShow Title is missing.
+ */
 router.post('/', shows.addShow);
 
 module.exports = router;
